ee0b2d3ed7120ead9c2e096022596dd48733c101
chrisclarkson <cc16956@essex.ac.uk>
2017-12-06 20:28:02 +0000

    Add files via upload

---
+++ smooth_artefacts.R
@@ -0,0 +1,23 @@
+library(shiny)
+ui <- basicPage(
+  selectInput("select", "Select column to plot and manipulate", choices=names(data)),
+  plotOutput("plot1", click = "plot_click", brush = "plot_brush"),
+  actionButton('button', 'button')
+)
+
+server <- function(input, output) {
+  
+  my <- reactiveValues(df=data) # Initialize df
+  
+  output$plot1 <- renderPlot({plot(my$df[, input$select], type = 'l')})
+  
+  observeEvent(input$plot_brush,{
+    rowmin <- round(input$plot_brush$xmin)
+    rowmax <- round(input$plot_brush$xmax)
+    my$df[rowmin:rowmax, input$select] <- mean(my$df[c(c(rowmin-1),c(rowmax+1)), input$select])
+  })
+  observeEvent(input$button, {
+    isolate(write.table(my$df,"./corrected.txt", sep='\t', quote=F, col.names = T))})
+}
+
+shinyApp(ui, server)
